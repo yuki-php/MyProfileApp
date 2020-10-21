1cd9864c7608fd534567a91db3bbd0f246778767
yknb-php <yuki_wtnb0121@icloud.com>
2020-10-22 03:16:06 +0900

    fix

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -24,7 +24,7 @@ class ProfileController extends Controller
 
     public function create()
     {
-        return view('index');
+        return view('profile');
     }
 
     public function store(Request $request)
