a9effc5246e44e4fb9e92378b6e9061ca3bfae04
yknb-php <yuki_wtnb0121@icloud.com>
2020-11-08 18:18:55 +0900

    検索機能＆SNSボタン追加

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -20,11 +20,12 @@
           <a href="{{ route('edit', ['id' => $profile->id ]) }}" class='btn btn-success'>編集</a>
           <a href="{{ route('delete', ['id' => $profile->id ]) }}" class='btn btn-danger'>削除</a>
         </div>
-@if(!empty($profile->image))
-        <img class='book-image' src="{{ asset('storage/images/'.$profile->image) }}">
-@else
-        <img class='book-image' src="{{ asset('images/dummy.jpeg') }}">
-@endif
+        
+        @if(!empty($profile->image))
+          <img class='book-image' src="{{ asset('storage/images/'.$profile->image) }}">
+        @else
+          <img class='book-image' src="{{ asset('images/dummy.jpeg') }}">
+        @endif
       </aside>
     </div>
     <a href="{{ route('index') }}" class='btn btn-info btn-back mb20'>一覧へ戻る</a>
